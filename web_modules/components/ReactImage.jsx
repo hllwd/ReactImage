9c878b689fa0c777df6eea4cee07e65e6f136365
hllwd <nicolasmondon@gmail.com>
2015-04-19 23:38:10 +0200

    - Simplify test on src props

--- web_modules/components/ReactImage.jsx
+++ web_modules/components/ReactImage.jsx
@@ -2,15 +2,13 @@
 
 
 var ReactImage = React.createClass({
-    render: function(){
+    render: function () {
 
-        var html = '<image ' + Object.keys(this.props).map(function(k){
-            if(k === 'src'){
-                return 'xlink:href="' + this.props[k] + '"'
-            }
-            return k + '="' + this.props[k] + '"'
-        }, this).join(' ') + '/>'
-
+        var html = '<image ' + Object.keys(this.props).map(function (k) {
+                return k === 'src'
+                    ? 'xlink:href="' + this.props[k] + '"'
+                    : k + '="' + this.props[k] + '"'
+            }, this).join(' ') + '/>'
         return (
             <g dangerouslySetInnerHTML={{__html: html}}></g>
         )
