b35cbc89bd1971ac7926d71bbf565fe8fc476251
hllwd <nicolasmondon@gmail.com>
2015-04-19 16:05:18 +0200

    - Manage props, the dirty way

---
+++ web_modules/components/ReactImage.jsx
@@ -0,0 +1,22 @@
+'use strict'
+
+
+var ReactImage = React.createClass({
+    render: function(){
+
+        var html = '<image ' + Object.keys(this.props).map(function(k){
+            if(k === 'src'){
+                return 'xlink:href="' + this.props[k] + '"'
+            }
+            return k + '="' + this.props[k] + '"'
+        }, this).join(' ') + '/>'
+
+        return (
+            <g dangerouslySetInnerHTML={{__html: html}}>
+            </g>
+        )
+    }
+})
+
+
+module.exports = ReactImage
--- web_modules/main.jsx
+++ web_modules/main.jsx
@@ -1,8 +1,8 @@
 'use strict'
 
-var ReactImage = require('./ReactImage')
+var ReactImage = require('./components/ReactImage')
 
 React.render(
-    <ReactImage />,
+    <ReactImage src="http://insolitebuzz.fr/wp-content/uploads/2014/10/test-all-the-things.jpg" width="100%" height="100%"/>,
     document.querySelector('svg')
 )
